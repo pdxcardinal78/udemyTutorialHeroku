9858a5f257002ffa78f5d431a27967dd324d68e8
Ryan Johnson <ryanj.johnson@gmail.com>
2020-10-05 23:22:19 -0700

    updated greeting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ greetings: "human" });
 });
 
 const PORT = process.env.PORT || 5000;
