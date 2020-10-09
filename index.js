798b5b39a1ed455ddf7bf05803c89089df05c349
Ryan Johnson <ryanj.johnson@gmail.com>
2020-10-09 14:41:16 -0700

    Updated index.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const keys = require("../server/config/keys.js");
+const keys = require("./config/keys");
 require("./models/user");
 require("./services/passport");
 const authRoutes = require("./routes/authroutes");
